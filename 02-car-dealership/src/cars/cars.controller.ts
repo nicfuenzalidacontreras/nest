ab2020c3566e52940db1b21c76e25aa5d1df652c
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-08-10 12:44:25 -0400

    primeros pasos con nest parte dos

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -13,7 +13,28 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById(@Param('id') id:string) {
-        return this.carsService.findOneById(Number(id));
+    getCarById(@Param('id', ParseIntPipe) id:number) {
+        return this.carsService.findOneById(id);
+    }
+
+    @Post()
+    createCar( @Body() body: any ) {
+      return body;
+    }
+  
+    @Patch(':id')
+    updateCar( 
+      @Param('id', ParseIntPipe) id:number, 
+      @Body() body: any ) 
+    {
+      return body;
+    }
+  
+    @Delete(':id')
+    deleteCar( @Param('id', ParseIntPipe ) id:number ) {
+      return {
+        method: 'delete',
+        id
+      };
     }
 }
