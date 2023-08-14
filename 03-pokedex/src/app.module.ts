000203ed09f1619662fe4248c44d2fb6ebbfe38f
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-08-14 17:36:37 -0400

    mongodb-pokedex parte dos

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
+      ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'), }),
-    PokemonModule
+      MongooseModule.forRoot('mongodb:://localhost:27017/nest-pokemon'),      
+      PokemonModule
   ],})
 export class AppModule {}
