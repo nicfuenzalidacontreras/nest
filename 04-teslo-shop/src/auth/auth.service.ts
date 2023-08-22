3c76c64e6372f08c9e7c125e28e0559808df4999
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-08-22 17:03:14 -0400

    autenticación de autorización parte uno

---
+++ 04-teslo-shop/src/auth/auth.service.ts
@@ -0,0 +1,70 @@
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { LoginUserDto, CreateUserDto } from './dto';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository:Repository<User>,
+    private readonly jwtService:JwtService,
+  ){}
+
+  async create(createUserDto: CreateUserDto) {
+    try{
+      const {password,...userData} = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password,10),
+      });
+      await this.userRepository.save(user);
+      delete user.password;
+
+      return {
+        ...user,
+        token:this.getJwtToken({email:user.email})
+      };
+    }catch (error) {
+      this.handleDBerrors(error);
+    }
+  }
+
+  async login(loginUserDto:LoginUserDto) {
+    try {
+      const {password, email} = loginUserDto;
+      const user = await this.userRepository.findOne({
+        where: {email},
+        select: {email: true, password: true}
+      });
+
+      if(!user)
+        throw new UnauthorizedException('Credentials are not valid (email)');
+
+      if(!bcrypt.compareSync(password, user.password))
+        throw new UnauthorizedException('Credentials are not valid (password)');
+
+      return {
+        ...user,
+        token:this.getJwtToken({email:user.email})
+      };
+    } catch (error) {
+      this.handleDBerrors(error);
+    }
+  }
+
+  private getJwtToken(payload:JwtPayload) {
+    return this.jwtService.sign(payload);
+  }
+
+  private handleDBerrors(error:any):never {
+    if(error.code === '23505')
+      throw new BadRequestException(error.detail);
+
+    throw new InternalServerErrorException('Please Check server logs');  
+  }
+}
